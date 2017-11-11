96cc7814e737433daeb8ac882897de932cb0225e
Simon Zea <simonzea@hotmail.com>
2017-11-11 09:46:20 -0500

    formulario del Perfil

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -2,25 +2,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //Recursos
+import { Button, Glyphicon,Table, Panel, Grid, Row, Col, FormGroup, ControlLabel, Label, FormControl } from 'react-bootstrap';
+import Popup from '../Extras/Pop-up';
+
+const loginStyles = {
+  width: "60%",
+  maxWidth: "400px",
+  margin: "20px auto",
+  border: "1px solid #ddd",
+  borderRadius: "5px",
+  padding: "10px"
+};
 
 
 
 class Perfil extends Component {
-  constructor(){
-    super();
-    this.Perfildata = this.Perfildata.bind(this);
-  }
-
-  Perfildata(event) {
-    event.preventDefault();
-    const email = this.correoInput.value;
-    const password = this.contrasenaInput.value;
-    this.loginFormulario.reset();
-    const usuario = {
-      email,
-      password
-    }
-  }
 
 
 
@@ -28,10 +24,26 @@ class Perfil extends Component {
     return (
       <div className="Perfil">
         <div className = "titulo text-center">
-          <h3>{this.props.User.email}</h3>
+          <h3>Perfil</h3>
         </div>
-        <h1>Perfil</h1>
-        
+        <div style = {loginStyles}>
+          <form ref = {(form) => {this.loginForm = form}}>
+            <FormGroup  >
+              <ControlLabel><Label>Nombre de Usuario</Label></ControlLabel>
+              <FormControl type = "text" placeholder = {this.props.User.displayName} inputRef = {(input) => {this.nameProductInput = input}}/>
+              <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup >
+              <ControlLabel><Label>Contraseña de Usuario</Label></ControlLabel>
+              <FormControl type = "text" placeholder = "Colocar nueva contraseña" inputRef = {(input) => {this.descProductInput = input}}/>
+              <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup className = "text-center">
+              <Button  ref = "target" bsStyle="success" type="button" onClick={this.handleAddProduct}> Actualizar el Perfil </Button>
+              <Popup/>
+            </FormGroup>
+          </form>
+        </div>
       </div>
     );
   }
