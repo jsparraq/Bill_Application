757846633bffa64076e987734ba8697d12982cb9
Sebastian <jsparraq@unal.edu.co>
2017-11-15 06:41:58 -0500

    Funciona la factura si no se repiten productos

--- src/components/Factura/index.jsx
+++ src/components/Factura/index.jsx
@@ -1,17 +1,110 @@
-//Dependencias
 import React, { Component } from 'react';
+import { Button, Glyphicon, Panel, Table,Grid,Row,Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
-//Recursos
+import { ADDTOCART, REMOVETOCART } from '../../Actions/producto';
+import { ERROR } from '../../Actions/error';
 
-//Actions
+const loginStyles = {
+  Galeria1 : {
+    'display': 'flex',
+    'flex-wrap': 'wrap',
+    'max-width': '1100px',
+    'margin-right': 'auto',
+    'margin-left': 'auto'
+  },
+  Producto1 : {
+    'box-sizing': 'border-box',
+    'margin': '1em',
+    'overflow': 'hidden',
+    'width': '150px',
+    'border-radius': '8px',
+    'position': 'relative'
+  },
+  footer: {
+    'fontWeight': 'bold'
+  }
+};
+
 class Factura extends Component {
+  constructor() {
+    super();
+    this.addToCart = this.addToCart.bind(this);
+  }
+
   render() {
     return (
-      <div className="Page404">
-        <h1>Page 404</h1>
+      <div>
+        <Grid>
+          <Row>
+            <Col sm={8}>
+              <div className = "Galeria" style = {loginStyles.Galeria1}>
+                {this.props.products.map(product =>
+                  <div className="thumbnail" key={product.id} style = {loginStyles.Producto1}>
+                    <img src={product.imagen} alt={product.nombre} />
+                    <div className="caption">
+                      <h4>{product.nombre}</h4>
+                      <p>
+                        <Button bsStyle="primary" onClick={() => this.props.ADDTOCART(product)} role="button" disabled={product.inventory <= 0}>${product.price} <Glyphicon glyph="shopping-cart" /></Button>
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <Panel header="Shopping Cart">
+                <Table fill>
+                  <tbody>
+                    {this.props.Cart.map(product =>
+                      <tr key={product.id}>
+                        <td>{product.nombre}</td>
+                        <td className="text-right">${product.precio}</td>
+                        <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.REMOVETOCART(product)}><Glyphicon glyph="trash" /></Button></td>
+                      </tr>
+                    )}
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td colSpan="4" style={loginStyles.footer}>
+                        Total: ${this.props.Cart.reduce((sum, product) => sum + Number(product.precio), 0)}
+                      </td>
+                    </tr>
+                  </tfoot>
+                </Table>
+              </Panel>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
+
+  addToCart(product) {
+
+  }
 }
 
-export default Factura;
+const mapStateToProps = state => {
+  return {
+    products: state.Productos,
+    Cart: state.Cart
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    ADDTOCART(producto){
+      dispatch(ADDTOCART(producto))
+    },
+    ERROR(message){
+      dispatch(ERROR(message));
+    },
+    REMOVETOCART(producto){
+      dispatch(REMOVETOCART(producto))
+    }
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Factura);
